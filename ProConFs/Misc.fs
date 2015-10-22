3d6459aafa4edfb233b07eb963f57ce7c6b97f49
vain0 <id_vain@yahoo.co.jp>
2015-10-22 23:49:29 +0900

    [misc] Add Option.operate and Ord.Down<'a>

---
+++ ProConFs/Misc.fs
@@ -0,0 +1,30 @@
+﻿module Misc
+
+open System
+open System.Collections.Generic
+
+module Option =
+  /// None を単位元とみなして演算する。
+  let operate f l r =
+      match (l, r) with
+      | (None, x)
+      | (x, None) -> x
+      | (Some l, Some r) -> f l r
+
+[<AutoOpen>]
+module Ord =
+  [<StructuralEquality>]
+  [<CustomComparison>]
+  /// Value with reversed order
+  type Down<'a when 'a: comparison> = | Down of 'a
+    with
+      member this.Value =
+          match this with
+          | Down x -> x
+      interface IComparable with
+        member this.CompareTo (rhs) =
+            match rhs with
+            | :? Down<'a> as rhs -> (this :> IComparable<Down<'a>>).CompareTo(rhs)
+            | _ -> invalidArg "rhs" "Type mismatch"
+      interface IComparable<Down<'a>> with
+        member this.CompareTo (rhs) = - compare (this.Value) (rhs.Value)
